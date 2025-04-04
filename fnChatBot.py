bb814684ab28f8b79fca1370b886ea573f9459c7
dch9610 <dch9610@naver.com>
2025-04-04 09:15:31 +0900

    ChatBot Ai 답변 남기게 하기 제너레이터에서 내용을 문자열로 변환

--- fnChatBot.py
+++ fnChatBot.py
@@ -6,10 +6,14 @@ from fnLLM import get_ai_response
 
 def fnChatBot():
     load_dotenv()
-    # 홈으로 돌아가기 버튼
+    # # 홈으로 돌아가기 버튼
     if st.button("홈으로 돌아가기",key="to_home"):
         go_to_page("Home")
         st.rerun() # 2번 클릭 방지
+
+    if st.button("초기화",key="rerun"):
+        st.session_state.message_list = []
+    
 
     if 'message_list' not in st.session_state:
         st.session_state.message_list = []
@@ -24,9 +28,9 @@ def fnChatBot():
         st.session_state.message_list.append({"role":"user", "content":user_question})
 
         with st.spinner("답변을 생성 중입니다."):
-            ai_message = get_ai_response(user_question)
-            #ai_message = "AI"
+            ai_stream = get_ai_response(user_question)  
+            ai_message = ''.join(ai_stream)  # 제너레이터에서 내용을 문자열로 변환  
             with st.chat_message("ai"):
                 st.write(ai_message)
             st.session_state.message_list.append({"role":"ai", "content":ai_message})
-    print("ChatBot")
+    print(f"after == {st.session_state.message_list}")
